d16217bd07ca342c2bb0c6c7bca4075e8592355a
Andres Pagliano <andres.pagliano@lavandaink.com.ar>
2012-04-03 12:29:59 -0300

    Added license in all files

--- src/test-suite/simple-aiml-engine-unit-test/simpleaimlengineunittest.cpp
+++ src/test-suite/simple-aiml-engine-unit-test/simpleaimlengineunittest.cpp
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2012 Andres Pagliano, Gabriel Miretti, Gonzalo Buteler,
+ * Nestor Bustamante, Pablo Perez de Angelis
+ *
+ * This file is part of LVK Botmaster.
+ *
+ * LVK Botmaster is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LVK Botmaster is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LVK Botmaster.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <QtCore/QString>
 #include <QtTest/QtTest>
 
