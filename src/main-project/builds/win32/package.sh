3e2d4a1e73a768777515e8bc7102874655475b6d
Andres Pagliano <andres.pagliano@lavandaink.com.ar>
2012-07-02 23:10:47 -0300

    Updated package.sh to include lang dir

--- src/main-project/builds/win32/package.sh
+++ src/main-project/builds/win32/package.sh
@@ -16,9 +16,9 @@ else
   rm -f $pkg_dir/*
 
   cp -v runtime/* $pkg_dir
-  
   cp -v .build/release/chatbot.exe $pkg_dir
   cp -v ../../CHANGELOG.txt $pkg_dir/CHANGELOG.txt
+  cp -vR ../../lang $pkg_dir
 
   rm -f sadosky-win32-$1.zip
   bin/7z.exe a sadosky-win32-$1.zip $pkg_dir/
