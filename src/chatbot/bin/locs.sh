91b9881df67601d20af9f6b3397d72876d2d5ee3
Andres Pagliano <andres.pagliano@lavandaink.com.ar>
2012-08-30 11:00:34 -0300

    Improved script locs.sh

--- src/chatbot/bin/locs.sh
+++ src/chatbot/bin/locs.sh
@@ -8,4 +8,17 @@ if ! [ -f $main_project_pro ]; then
   exit 1
 fi
 
+rm -rf ../test-suite/.test-suite-shadow-build/
+
+echo -n "Chatbot lines of code with comments       : "
+cat `find . -name "*.cpp" -or -name "*.h"` | grep -v "^\\s*$" | wc -l
+echo -n "Chatbot lines of code without comments    : "
+cat `find . -name "*.cpp" -or -name "*.h"` | grep -v "^\\s*$\|^\\s*\(//\|/\\*\|\\*\|\\*/\).*$" | wc -l
+echo -n "Test suite lines of code with comments    : "
+cat `find ../test-suite -name "*.cpp" -or -name "*.h"` | grep -v "^\\s*$" | wc -l
+echo -n "Test suite lines of code without comments : "
+cat `find ../test-suite -name "*.cpp" -or -name "*.h"` | grep -v "^\\s*$\|^\\s*\(//\|/\\*\|\\*\|\\*/\).*$" | wc -l
+echo -n "Total lines of code with comments         : "
 cat `find . ../test-suite -name "*.cpp" -or -name "*.h"` | grep -v "^\\s*$" | wc -l
+echo -n "Total lines of code without comments      : "
+cat `find . ../test-suite -name "*.cpp" -or -name "*.h"` | grep -v "^\\s*$\|^\\s*\(//\|/\\*\|\\*\|\\*/\).*$" | wc -l
