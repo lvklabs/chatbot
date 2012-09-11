02fee5993663efbd643374b3a165982eb3379b27
Andres Pagliano <andres.pagliano@lavandaink.com.ar>
2012-09-11 12:40:28 -0300

    Invoking cccc in bin/locs.sh

--- src/chatbot/bin/locs.sh
+++ src/chatbot/bin/locs.sh
@@ -10,6 +10,8 @@ fi
 
 rm -rf ../test-suite/.test-suite-shadow-build/
 
+find front-end/ back-end/ nlp-engine/ stats/ common/ chat-adapter/ -iname "*.h" -or -iname "*.cpp" -exec cccc {} \+
+
 echo -n "Chatbot lines of code with comments       : "
 cat `find . -name "*.cpp" -or -name "*.h"` | grep -v "^\\s*$" | wc -l
 echo -n "Chatbot lines of code without comments    : "
@@ -22,3 +24,4 @@ echo -n "Total lines of code with comments         : "
 cat `find . ../test-suite -name "*.cpp" -or -name "*.h"` | grep -v "^\\s*$" | wc -l
 echo -n "Total lines of code without comments      : "
 cat `find . ../test-suite -name "*.cpp" -or -name "*.h"` | grep -v "^\\s*$\|^\\s*\(//\|/\\*\|\\*\|\\*/\).*$" | wc -l
+
