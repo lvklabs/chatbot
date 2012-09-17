94b35ea63b23db1ad75f46ca83a52096ac278ba5
Andres Pagliano <andres.pagliano@lavandaink.com.ar>
2012-09-17 13:14:07 -0300

    Updated link position in welcome window for win32

--- src/chatbot/front-end/welcomewidget.cpp
+++ src/chatbot/front-end/welcomewidget.cpp
@@ -28,6 +28,7 @@ Lvk::FE::WelcomeWidget::WelcomeWidget(QWidget *parent) :
 
 #ifdef WIN32
     ui->spacerTop->changeSize(20, 214);
+    ui->linkSpacer->changeSize(20, 64);
 #endif
 
     setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter, size(),
