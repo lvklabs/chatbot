ba549542db061d94a34d8a487e292fce0a756d81
Andres Pagliano <andres.pagliano@lavandaink.com.ar>
2012-09-18 09:42:00 -0300

    Added new test cases to SecureStatsFileTest

--- src/test-suite/secure-stats-file-unit-test/tst_securestatsfiletest.cpp
+++ src/test-suite/secure-stats-file-unit-test/tst_securestatsfiletest.cpp
@@ -5,6 +5,7 @@
 
 #include "stats/securestatsfile.h"
 #include "stats/metric.h"
+#include "stats/score.h"
 
 #define FILENAME_1 "teststatfile1.stat"
 
@@ -28,8 +29,24 @@ private Q_SLOTS:
     void cleanupTestCase();
     void testIsEmptyAndClose();
     void testIsEmptySaveAndClear();
-    void testSetSaveAndLoad();
+    void testSaveAndLoadScore();
+    void testSaveAndLoadMetrics();
     void testNewIntervalAndHistory();
+
+private:
+
+    void verifyAllEmpty(const Stats::SecureStatsFile &file)
+    {
+        QVERIFY(file.isEmpty());
+        QVERIFY(file.currentScore().isNull());
+        QVERIFY(file.bestScore().isNull());
+        QVERIFY(file.scoreElapsedTime() == 0);
+        QVERIFY(file.contacts().isEmpty());
+
+        Cmn::Conversation h;
+        file.chatHistory(h);
+        QVERIFY(h.isEmpty());
+    }
 };
 
 //--------------------------------------------------------------------------------------------------
@@ -63,11 +80,11 @@ void SecureStatsFileTest::testIsEmptyAndClose()
 {
     Stats::SecureStatsFile file(FILENAME_1);
 
-    QVERIFY(file.isEmpty());
+    verifyAllEmpty(file);
     file.setMetric(Stats::LexiconSize, QVariant(10));
     QVERIFY(!file.isEmpty());
     file.close();
-    QVERIFY(file.isEmpty());
+    verifyAllEmpty(file);
     QVERIFY(!QFile::exists(FILENAME_1));
 }
 
@@ -77,20 +94,134 @@ void SecureStatsFileTest::testIsEmptySaveAndClear()
 {
     Stats::SecureStatsFile file(FILENAME_1);
 
-    QVERIFY(file.isEmpty());
+    verifyAllEmpty(file);
     file.setMetric(Stats::LexiconSize, QVariant(10));
     QVERIFY(!file.isEmpty());
     file.save();
     QVERIFY(QFile::exists(FILENAME_1));
     file.clear();
-    QVERIFY(file.isEmpty());
+    verifyAllEmpty(file);
     QVERIFY(!QFile::exists(FILENAME_1));
 }
 
 
 //--------------------------------------------------------------------------------------------------
 
-void SecureStatsFileTest::testSetSaveAndLoad()
+void SecureStatsFileTest::testSaveAndLoadScore()
+{
+
+    Stats::Score cs(10, 20, 30);
+    Stats::Score bs(100, 200, 300);
+    int et = 32134;
+
+    QString c1 = "John";
+    QString c2 = "Jane G";
+    QString c3 = "John";
+
+    Cmn::Conversation::Entry e1(QDateTime::currentDateTime(), c1, c3, "Hi!!!", "Hi!", true, 2);
+    Cmn::Conversation::Entry e2(QDateTime::currentDateTime(), c1, c3, "What's up?", "", false, 0);
+
+    {
+        Stats::SecureStatsFile file(FILENAME_1);
+
+        verifyAllEmpty(file);
+
+        file.setCurrentScore(cs);
+        file.setBestScore(bs);
+        file.setScoreElapsedTime(et);
+
+        QVERIFY(!file.isEmpty());
+
+        file.addContact(c1);
+        file.addContact(c2);
+        file.addContact(c3);
+
+        file.appendChatEntry(e1);
+        file.appendChatEntry(e2);
+        file.appendChatEntry(e2);
+
+        QVERIFY(file.currentScore() == cs);
+        QVERIFY(file.bestScore() == bs);
+        QVERIFY(file.scoreElapsedTime() == et);
+        QVERIFY(file.contacts().size() == 2);
+        QVERIFY(file.contacts().contains(c1));
+        QVERIFY(file.contacts().contains(c2));
+
+        Cmn::Conversation h;
+        file.chatHistory(h);
+        QVERIFY(h.size() == 3);
+        QVERIFY(h.entries().at(0) == e1);
+        QVERIFY(h.entries().at(1) == e2);
+        QVERIFY(h.entries().at(2) == e2);
+
+        file.save();
+        QVERIFY(QFile::exists(FILENAME_1));
+    }
+
+    {
+        Stats::SecureStatsFile file;
+
+        file.load(FILENAME_1);
+
+        QVERIFY(!file.isEmpty());
+
+        QVERIFY(file.currentScore() == cs);
+        QVERIFY(file.bestScore() == bs);
+        QVERIFY(file.scoreElapsedTime() == et);
+        QVERIFY(file.contacts().size() == 2);
+        QVERIFY(file.contacts().contains(c1));
+        QVERIFY(file.contacts().contains(c2));
+
+        Cmn::Conversation h;
+        file.chatHistory(h);
+        QVERIFY(h.size() == 3);
+        QVERIFY(h.entries().at(0) == e1);
+        QVERIFY(h.entries().at(1) == e2);
+        QVERIFY(h.entries().at(2) == e2);
+
+        // interval 2 ////////////////////////////////
+
+        file.newInterval();
+
+        QVERIFY(file.currentScore() == cs);
+        QVERIFY(file.bestScore() == bs);
+        QVERIFY(file.scoreElapsedTime() == 0);
+        QVERIFY(file.contacts().size() == 2);
+        QVERIFY(file.contacts().contains(c1));
+        QVERIFY(file.contacts().contains(c2));
+
+        h.clear();
+        file.chatHistory(h);
+        QVERIFY(h.isEmpty());
+
+        // interval 3 ////////////////////////////////
+
+        file.newInterval();
+
+        QVERIFY(file.currentScore() == cs);
+        QVERIFY(file.bestScore() == bs);
+        QVERIFY(file.scoreElapsedTime() == 0);
+        QVERIFY(file.contacts().size() == 2);
+        QVERIFY(file.contacts().contains(c1));
+        QVERIFY(file.contacts().contains(c2));
+
+        h.clear();
+        file.chatHistory(h);
+        QVERIFY(h.isEmpty());
+
+        file.clear();
+    }
+
+    {
+        Stats::SecureStatsFile file(FILENAME_1);
+
+        verifyAllEmpty(file);
+    }
+}
+
+//--------------------------------------------------------------------------------------------------
+
+void SecureStatsFileTest::testSaveAndLoadMetrics()
 {
     const Stats::Metric m1 = Stats::LexiconSize;
     const Stats::Metric m2 = Stats::HistoryChatbotDiffLines;
@@ -153,6 +284,16 @@ void SecureStatsFileTest::testSetSaveAndLoad()
         QVERIFY(v.toUInt() == value3);
         file.metric(m4, v);
         QVERIFY(v.toUInt() == (value4a + value4b)*ITERATIONS);
+
+        file.clear();
+    }
+
+    {
+        Stats::SecureStatsFile file;
+
+        file.load(FILENAME_1);
+
+        verifyAllEmpty(file);
     }
 }
 
