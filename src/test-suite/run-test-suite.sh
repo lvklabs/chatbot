96753ff5b500af0f47b506cbfc1305d46976f1cc
Andres Pagliano <andres.pagliano@lavandaink.com.ar>
2012-09-19 10:19:50 -0300

    Added option to run-test-suite.sh to run only one test

--- src/test-suite/run-test-suite.sh
+++ src/test-suite/run-test-suite.sh
@@ -4,23 +4,29 @@ shadow_build_dir=.test-suite-shadow-build
 project_dir=..
 log_file=run.log
 
+run_unit=no
+run_e2e=no
+run_single=no
+
 if [ "$1" == "-u" ]; then
   run_unit=yes
-  run_e2e=no
   config="CONFIG+=unit_tests"
 elif [ "$1" == "-e" ]; then
-  run_unit=no
   run_e2e=yes
   config="CONFIG+=end_to_end_tests"
 elif [ "$1" == "-a" ]; then
   run_unit=yes
   run_e2e=yes
   config="CONFIG+=unit_tests end_to_end_tests"
+elif [ -d "$1" ]; then
+  run_single=yes
+  config="SUBDIRS+=$1"
 else
   echo "Syntax:"
-  echo "   $0 -u   # Run unit and system tests"
-  echo "   $0 -e   # Run end to end tests"
-  echo "   $0 -a   # Run all tests"
+  echo "   $0 -u        # Run unit and system tests"
+  echo "   $0 -e        # Run end to end tests"
+  echo "   $0 -a        # Run all tests"
+  echo "   $0 test_name # Run only specified test"
   exit 1
 fi
 
@@ -46,6 +52,11 @@ fi
 if [ "$run_e2e" == "yes" ]; then
   ( cd end-to-end-test && ./endToEndTest | tee -a ../$log_file )
 fi
+
+if [ "$run_single" == "yes" ]; then
+  ( cd $1 && find . -name "*Test" -executable -exec ./{} \; | tee -a ../$log_file )
+fi
+
 
 echo
 echo
