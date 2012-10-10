2d023965d48313164ff376c3cff58b06c269379d
Andres Pagliano <andres.pagliano@lvklabs.com>
2012-10-09 23:32:12 -0300

    Set version 0.26

--- src/chatbot/common/version.h
+++ src/chatbot/common/version.h
@@ -27,7 +27,7 @@
 #define ORGANIZATION_NAME   "LVK"
 #define ORGANIZATION_DOMAIN "lvklabs.com"
 
-#define APP_VERSION_STR     "0.26.UNSTABLE"
+#define APP_VERSION_STR     "0.26"
 #define APP_VERSION_REV     AUTO_APP_VERSION_REV
 #define APP_NAME            "Chatbot"
 #define APP_URL_CONTACT     "www.lvklabs.com"
