4900a144cc0c7c8f72da9bf38f99e80c106f456d
Andres Pagliano <andres.pagliano@lvklabs.com>
2013-03-22 12:20:15 -0300

    Set version 2.0-clue-beta3

--- src/chatbot/common/version.h
+++ src/chatbot/common/version.h
@@ -27,7 +27,7 @@
 #define ORGANIZATION_NAME   "LVK"
 #define ORGANIZATION_DOMAIN "lvklabs.com"
 
-#define APP_VERSION_STR     "2.0-clue-beta2"
+#define APP_VERSION_STR     "2.0-clue-beta3"
 #define APP_VERSION_REV     AUTO_APP_VERSION_REV
 #define APP_NAME            "Chatbot"
 #define APP_URL_CONTACT     "www.lvklabs.com"
