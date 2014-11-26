2f0db3dc660f250e99fb57ac05236ef71f4d0ec4
Andres Pagliano <andres.pagliano@lvklabs.com>
2014-11-25 16:31:18 -0800

    Disabled pkg upload from build scripts

--- da_release/release.sh
+++ da_release/release.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-function show_usage
-{
-  echo "Usage:" `basename $0` "[ --force ]"
-}
-
 if uname | grep -qi linux; then
   target_os=linux32
   pkg_format=tar.gz
@@ -41,22 +36,10 @@ pkg_version="$pkg_version-clue"
 pkg_name="$pkg_name-clue"
 pkg_filename=$pkg_name.$pkg_format
 
-echo "Verifying if $pkg_filename already exists at $lvk_host/$pkg_host_path..."
-if ssh $lvk_user@$lvk_host "test -f $pkg_host_path/$pkg_filename"; then
-  if [ "$1" != "--force" ]; then
-    echo "Error: $pkg_filename already exists. To overwrite use --force"
-    exit 1
-  else
-    echo "$pkg_filename already exists but overwriting"
-  fi
-fi
-
 pushd $target_os
 
 ./build.sh && \
 ./package.sh $pkg_version && \
-./upload.sh $pkg_filename && \
-git tag -f $pkg_version && \
-echo "*** Everything went OK! Package uploaded and repository tagged ***"
+echo "*** Everything went OK! ***"
 
 popd
